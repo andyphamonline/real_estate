80ac63908f756a93832ec057ccaff8b17a3f58a7
theking9985 <theking9985@yahoo.com>
2015-12-14 20:35:22 -0800

    1st API works, chaining .then to make 2nd API

--- controllers/search.js
+++ controllers/search.js
@@ -6,14 +6,9 @@ var request = require("request");
 // API GetSearchResults works!!!
 router.get("/", function(req, res) {
 	var url = 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=' + process.env.ZILLOW_KEY + "&address=" + req.query.address + "&citystatezip=" + req.query.citystatezip;
-	console.log(url);
   request(
     url,
     function(error, response, body) {
-
-    	console.log("*****************");
-      console.log(req.query.address);
-      console.log(req.query.citystatezip);
       // console.log(body);
       if (!error && response.statusCode === 200) {        
         var parseString = require('xml2js').parseString;
@@ -22,13 +17,30 @@ router.get("/", function(req, res) {
           if (error) {
             console.log(error);
           }
-          console.log(result);
+          // console.log(result);
           res.send(result);
-          // var data = result["Comps:comps"].response[0].properties[0].comparables[0].comp;
-            
+          var data = result["SearchResults:searchresults"].response[0].results[0].result[0].zpid;
         });       
       }
     }
+  ).then(
+  	request(
+		'http://www.zillow.com/webservice/GetComps.htm?zws-id=' + process.env.ZILLOW_KEY + '&zpid=48749425&count=2',
+		function(error, response, body) {
+			if (!error && response.statusCode === 200) {				
+				var parseString = require('xml2js').parseString;
+				var xml = body;
+				parseString(xml, function (err, result) {
+					if (error) {
+						console.log(error);
+					}
+					var data = result["Comps:comps"].response[0].properties[0].comparables[0].comp;
+				    // res.render("index", {data:data});
+            res.send(result);
+				});				
+			}
+		}
+	)
   )
 });
 

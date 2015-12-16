1e03f44a789977466c885efc834c621bb3f50e76
theking9985 <theking9985@yahoo.com>
2015-12-15 17:41:04 -0800

    before buy property button

--- models/property.js
+++ models/property.js
@@ -1,10 +1,33 @@
 var mongoose = require("mongoose");
 
 var PropertySchema = new mongoose.Schema({
-	address: String,
-	propertyValue: Number,
 	zpid: Number,
+	address: String,
+	cityStateZip: String,
+	price: Number,
+	yearBuilt: Number,
+	lotSize: Number,
+	bedrooms: String,
+	bathrooms: String,
+	lastSoldPrice: Number
 	user: [{type: mongoose.Schema.Types.ObjectId, ref: 'user'}]
+});
+
+UserShema.set("toJSON", {
+	transform: function(doc, ret, options) {
+		var returnJson = {
+			id: ret._id,
+			zpid: ret.zpid,
+			address: ret.address,
+			cityStateZip: ret.cityStateZip,
+			yearBuilt: ret.yearBuilt,
+			lotSize: ret.lotSize,
+			bedrooms: ret.bedrooms,
+			bathrooms: ret.bathrooms,
+			lastSoldPrice: ret.lastSoldPrice
+		};
+		return returnJson;
+	}
 });
 
 module.exports = mongoose.model("Property", PropertySchema);
